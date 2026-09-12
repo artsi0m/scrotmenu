cbefb2e949b893cf1b9fe0707361fe42fe7c4629
Корякин Артём <karakin2000@gmail.com>
2026-09-12 22:12:26 +0300

    implemented main cycle

--- rofi-scrot-ui.py
+++ rofi-scrot-ui.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
-scrot_opts_proc = subprocess.Popen(['scrot', '--list-options=tsv'], stdout=subprocess.PIPE)
-rofi_input_barr = bytearray(scrot_opts_proc.stdout.read())
-rofi_input_barr.extend('Run scrot\n'.encode())
+from subprocess import run,Popen,PIPE
+
+menu_cmd = ['rofi', '-dmenu'] # dmenu compatible menu
+trig_exec_bytes = bytes('\tRun scrot\n'.encode()) # String that executes scrot when selected with menu
+
+scrot_opts_proc = Popen(['scrot', '--list-options=tsv'], stdout=PIPE)
+rofi_input_barr = bytearray()
+rofi_input_barr.extend(trig_exec_bytes)
+rofi_input_barr.extend(scrot_opts_proc.stdout.read())
+
+
+def exec_scrot():
+    pass
+
+while True:
+    rofi_comp_proc = run(menu_cmd, input=rofi_input_barr, capture_output=True)
+    if rofi_comp_proc.stdout == trig_exec_bytes:
+        exec_scrot()
+        break
